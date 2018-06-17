aed838737f7d4e4b459a915fc08516311c169d1a
Austin Euchner <aeuchner@gmail.com>
2018-06-17 17:49:47 -0500

    cross origin library for api calls

--- scripts/rs-scripts.js
+++ scripts/rs-scripts.js
@@ -1,10 +1,25 @@
+// function GetOverall(player){
+// 	// $('#body').load('http://google.com');
+// 	$.ajax({
+// 		url: 'http://services.runescape.com/m=hiscore/index_lite.ws?player=Ystin',
+// 		type: 'GET',
+// 		// headers: {'Access-Control-Allow-Origin': "x-requested-with"},
+// 		// dataType: "jsonp",
+// 		// xhrFields: {cors:false},
+//
+// 		success: function(result){
+// 			console.log(result);
+// 		}
+// 	});
+// }
+
 function GetOverall(player){
 	$.ajax({
-		url: 'http://services.runescape.com/m=hiscore/index_lite.ws?player=Ystin',
-		headers: {'Access-Control-Allow-Origin': "*"},
-		// dataType: "jsonp",
-		success: function(result){
-			console.log(result);
-		}
-	});
+  crossOrigin: true,
+  //proxy: "http://www.domain.com/path/proxy.php" -> to overide default proxy
+  url: 'http://services.runescape.com/m=hiscore/index_lite.ws?player=Ystin',
+  success: function(data) {
+	console.log(data);
+  }
+});
 }
